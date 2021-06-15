0f75346926d9f5dbb8178474919a7a59f39c4ede
Katerina Zamiatina <eozamyatina@gmail.com>
2021-06-15 09:13:16 +0300

    refactor: contacts validation

--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -1,47 +1,49 @@
 const Joi = require('joi');
 
+const addContactSchema = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).required(),
+
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net', 'org', 'ua', 'ru', 'gov'] },
+    })
+    .required(),
+
+  phone: Joi.string()
+    .pattern(/^([+]?\d{1,2}[-\s]?|)\d{3}[-\s]?\d{3}[-\s]?\d{4}$/)
+    .required(),
+});
+
+const updateContactSchema = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).optional(),
+
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net', 'org', 'ua', 'ru', 'gov'] },
+    })
+    .optional(),
+
+  phone: Joi.string()
+    .pattern(/^([+]?\d{1,2}[-\s]?|)\d{3}[-\s]?\d{3}[-\s]?\d{4}$/)
+    .optional(),
+}).min(1);
+
+const validate = (schema, req, res, next) => {
+  const validationResult = schema.validate(req.body);
+  if (validationResult.error) {
+    return res.status(400).json({ status: validationResult.error.details });
+  }
+  next();
+};
+
 module.exports = {
   addContactValidation: (req, res, next) => {
-    const addContactSchema = Joi.object({
-      name: Joi.string().alphanum().min(3).max(30).required(),
-
-      email: Joi.string()
-        .email({
-          minDomainSegments: 2,
-          tlds: { allow: ['com', 'net', 'org', 'ua', 'ru', 'gov'] },
-        })
-        .required(),
-
-      phone: Joi.string()
-        .pattern(/^([+]?\d{1,2}[-\s]?|)\d{3}[-\s]?\d{3}[-\s]?\d{4}$/)
-        .required(),
-    });
-    const validationResult = addContactSchema.validate(req.body);
-    if (validationResult.error) {
-      return res.status(400).json({ status: validationResult.error.details });
-    }
-    next();
+    return validate(addContactSchema, req, res, next);
   },
 
   updateContactValidation: (req, res, next) => {
-    const updateContactSchema = Joi.object({
-      name: Joi.string().alphanum().min(3).max(30).optional(),
-
-      email: Joi.string()
-        .email({
-          minDomainSegments: 2,
-          tlds: { allow: ['com', 'net', 'org', 'ua', 'ru', 'gov'] },
-        })
-        .optional(),
-
-      phone: Joi.string()
-        .pattern(/^([+]?\d{1,2}[-\s]?|)\d{3}[-\s]?\d{3}[-\s]?\d{4}$/)
-        .optional(),
-    }).min(1);
-    const validationResult = updateContactSchema.validate(req.body);
-    if (validationResult.error) {
-      return res.status(400).json({ status: validationResult.error.details });
-    }
-    next();
+    return validate(updateContactSchema, req, res, next);
   },
 };
