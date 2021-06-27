72d69ca2f23d4d63f06b86eadff94a4935598c6c
Katerina Zamiatina <eozamyatina@gmail.com>
2021-06-27 17:28:05 +0300

    "add: helmet&rateLimit"

--- app.js
+++ app.js
@@ -1,35 +1,29 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 
 const usersRouter = require('./src/routes/api/users');
 const contactsRouter = require('./src/routes/api/contacts');
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
 
 const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 const { errorHandler } = require('./src/helpers/apiHelpers');
-// const session = require('express-session');
-// const passport = require('passport');
 
 app.use(logger(formatsLogger));
+app.use(helmet());
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: 10000 }));
 
-// app.use(
-//   session({
-//     secret: 'secret-word',
-//     key: 'session-key',
-//     cookie: { path: '/', httpOnly: true, maxAge: null },
-//     saveUninitialized: false,
-//     resave: false,
-//   }),
-// );
-// require('./config/config-passport');
-// app.use(passport.initialize());
-// app.use(passport.session());
-
+app.use('/api/', apiLimiter);
 app.use('/api/users', usersRouter);
 app.use('/api/contacts', contactsRouter);
 
