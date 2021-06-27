4c6ec3d18f7a62eff6b3855550c89bd8f810e4d7
Katerina Zamiatina <eozamyatina@gmail.com>
2021-06-27 17:34:05 +0300

    add: guard (compare token from DB and current)

--- src/helpers/guard.js
+++ src/helpers/guard.js
@@ -1,14 +1,45 @@
-const passport = require('passport');
-require('../config/config-passport');
+// const passport = require('passport');
+// require('../config/config-passport');
+const jwt = require('jsonwebtoken');
+const { NotAuthorizedError } = require('../helpers/errors');
+const { findUserByToken } = require('../services/userService');
 
-const guard = (req, res, next) => {
-  passport.authenticate('jwt', { session: false }, (err, user) => {
-    if (err || !user) {
-      return res.status(401).json({ message: 'Not authorized' });
+const guard = async (req, res, next) => {
+  try {
+    const auth = req.headers['authorization'];
+
+    const [, token] = auth.split(' ');
+
+    if (!token) {
+      next(new NotAuthorizedError('Please, provide token'));
     }
-    req.user = user;
-    return next();
-  })(req, res, next);
+    const userDb = await findUserByToken(token);
+    const user = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  
+
+    if (userDb.id === user.id) {
+      req.token = token;
+      req.user = userDb;
+      console.log(req.user.id)
+      return next();
+    }
+    next(new NotAuthorizedError('Invalid token'));
+  } catch (error) {
+    next(new NotAuthorizedError('Invalid token'));
+  }
 };
+
+// const guard = (req, res, next) => {
+//   console.log('HEADERS', req.headers)
+//   passport.authenticate('jwt', { session: false }, (err, user) => {
+//     if (err || !user) {
+//       return next({ status: 'failure', message: 'Forbidden' });
+//     }
+
+//     req.user = user;
+
+//     return next();
+//   })(req, res, next);
+// };
 
 module.exports = { guard };
